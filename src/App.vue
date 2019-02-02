3992b93cbdb7f6b9f072f311462665f8c6545e13
Cherie Fu <cheriefu@fortinet.com>
2019-02-01 17:22:41 -0800

    add popover component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="popover-target" @mouseenter="mouseEnter" @mouseleave="mouseLeave">
+      <vu-popover content="Welcome to Your Vue.js App" :trigger="showPopover"/>hover this to popover
+    </span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vuPopover from "./components/vu-popover.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    vuPopover
+  },
+  data: function() {
+    return {
+      showPopover: false
+    };
+  },
+  methods: {
+    mouseEnter: function() {
+      console.log('mouse enter');
+      this.showPopover = true;
+    },
+    mouseLeave: function() {
+      this.showPopover = false;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.popover-target {
+  padding: 2px 8px;
+  border: 1px solid #cccccc;
+  background-color: bisque;
+  border-radius: 5px;
+  position: relative;
 }
 </style>
---
+++ src/components/vu-popover.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="vu-popover" v-if="isShow">
+    <div class="popover-container">{{content}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vu-popover",
+  props: ["content", "trigger"],
+  // data:function(){
+  //   return {
+  //     showPopover: false,
+  //   }
+  // },
+  computed: {
+    isShow: function(){
+      console.log('tigger changed', this.trigger)
+      return this.trigger;
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.vu-popover {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+
+}
+.popover-container {
+  background: #fff;
+  box-shadow: 0px 4px 20px 0px rgba(52, 73, 94, 0.2);
+  padding: 5px;
+  border-radius: 5px;
+  z-index: 998;
+}
+</style>
